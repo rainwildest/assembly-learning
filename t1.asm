a2821577f23f8b6f81d9ae94ec9c7b24b4bfe3d0
rainwildest <rainwildest.163.com>
2024-05-17 00:00:22 +0800

    编写实验10.3用例

--- t1.asm
+++ t1.asm
@@ -1,39 +1,93 @@
-assume cs:codesg
+assume cs:codesg,ds:datasg
+
+datasg segment
+	       db 10 dup(0)
+datasg ends
 
 codesg segment
-	start: mov  ax, 4243H	; 被除数的低16位
-	       mov  dx, 000FH	; 被除数的高16位
-	       mov  cx, 0AH  	; 除数
+	start:   mov  ax, 12666
+	         mov  bx, datasg
+	         mov  ds, bx
+	         mov  si, 0
+	         call dtoc
 
-	       call divdw    	; 调用子程序
+	         mov  dh, 8
+	         mov  dl, 3
+	         mov  cl, 2
+	         call show_str
 				 
-	       mov  ax, 4c00H
-	       int  21H
+	         mov  ax, 4c00H
+	         int  21H
+
+	dtoc:    
+	         push si
+	         mov  si, 0           	; 记录循环次数
+ 
+	         mov  cx, ax          	; 记录刚开始的被除数
+	modulus: 
+	         mov  dx, 0
+	         mov  di, 10
+	         div  di
+	        
+	         sub  cx, dx          	; 取得当前余数减去原始的被除数，看是否等于0，如果是0则说明该值已经被全部转换完成
+
+	         mov  di, dx
+	         add  di, 30H
+	         mov  ds:[si], di     	;将字符串写入 ds 中
+
+	         inc  si              	; 增加循环次数
+	         jcxz over
+
+	         mov  cx, ax          	; 没有得到0，则将当前的余数再次给到CX
+	         jmp  modulus
+
+	over:    
+	         pop  si
+	         ret
 
 
-	divdw: 
-	; X: 被除数，范围: [0,FFFFFFFF]
-	; N: 除数, 范围: [0,FFFF]
-	; H: X高16位，范围: [0,FFFF]
-	; L: X低16位，范围: [0,FFFF]
-	; int(): 描述性运算符，取商，比如 int(38/10)=3
-	; rem(): 描述性运算符，取余数，比如 rem(38/10)=8
-	; 公式: X/N = int(H/N)*65536 +  [rem(H/N)*65536+L]/N
-	       mov  bx, ax
-	       mov  ax, dx
-	       mov  dx, 0
-	       div  cx
+	show_str:
+	         push ax
+	         push bx
+	         push cx
+	         push es
+	         push di
 
-	       mov  si, ax
+	         mov  ax, 0b800H
+	         mov  es, ax
 
-	       mov  ax, bx
-	       div  cx
+	         mov  ax, 0
+	         mov  al, 160
+	         mul  dh              	; 相乘后结果会放入 ax 中
+	         mov  bx, ax          	; 处理行号
 
-	       mov  cx, dx
-	       mov  dx, si
+	         mov  ax, 0
+	         mov  al, 2
+	         mul  dl              	; 处理列
+					 
+	         add  bx, ax          	; 将行和列的积相加
 
-	       ret           	; pop IP
+	         mov  al, cl          	; 把颜色值给予 al
+	         mov  di, 0
 
+	display: 
+	         mov  cx, 0
+	         mov  cl, ds:[si]
+	         jcxz done
+
+	         mov  es:[bx+di], cl
+	         mov  es:[bx+di+1], al
+	         inc  si
+	         add  di,2
+	         jmp  short display
+
+	done:    
+	         pop  di
+	         pop  es
+	         pop  cx
+	         pop  bx
+	         pop  ax
+	         ret                  	; pop IP
 codesg ends
 
 end start
