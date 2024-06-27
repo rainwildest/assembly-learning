fa0924c550baabe9e91bb87f6d8f3d590a22c056
rainwildest <rainwildest.163.com>
2024-06-27 22:44:49 +0800

    添加实验12

--- t1.asm
+++ t1.asm
@@ -1,93 +1,80 @@
-assume cs:codesg,ds:datasg
+assume cs:codesg, ds:datasg
 
 datasg segment
 	       db 10 dup(0)
 datasg ends
 
 codesg segment
-	start:   mov  ax, 12666
-	         mov  bx, datasg
-	         mov  ds, bx
-	         mov  si, 0
-	         call dtoc
+	start:            
+	;  call divide_error
+	                  call copy_divide_error
+	                  call set_int0
 
-	         mov  dh, 8
-	         mov  dl, 3
-	         mov  cl, 2
-	         call show_str
+	                  int  0H
 				 
-	         mov  ax, 4c00H
-	         int  21H
-
-	dtoc:    
-	         push si
-	         mov  si, 0           	; 记录循环次数
- 
-	         mov  cx, ax          	; 记录刚开始的被除数
-	modulus: 
-	         mov  dx, 0
-	         mov  di, 10
-	         div  di
-	        
-	         sub  cx, dx          	; 取得当前余数减去原始的被除数，看是否等于0，如果是0则说明该值已经被全部转换完成
-
-	         mov  di, dx
-	         add  di, 30H
-	         mov  ds:[si], di     	;将字符串写入 ds 中
-
-	         inc  si              	; 增加循环次数
-	         jcxz over
-
-	         mov  cx, ax          	; 没有得到0，则将当前的余数再次给到CX
-	         jmp  modulus
-
-	over:    
-	         pop  si
-	         ret
+	                  mov  ax, 4c00H
+	                  int  21H
 
 
-	show_str:
-	         push ax
-	         push bx
-	         push cx
-	         push es
-	         push di
+	; 设置 int0 中断跳转的CS和IP
+	set_int0:         
+	                  mov  ax,0
+	                  mov  es,ax
 
-	         mov  ax, 0b800H
-	         mov  es, ax
+	                  mov  word ptr es:[0*4+0], 200H                        	;低位IP
+	                  mov  word ptr es:[0*4+2], 0                           	;高位CS
+	                  ret
 
-	         mov  ax, 0
-	         mov  al, 160
-	         mul  dh              	; 相乘后结果会放入 ax 中
-	         mov  bx, ax          	; 处理行号
+	copy_divide_error:
+	                  mov  ax, cs
+	                  mov  ds, ax
+	                  mov  si, OFFSET divide_error
 
-	         mov  ax, 0
-	         mov  al, 2
-	         mul  dl              	; 处理列
-					 
-	         add  bx, ax          	; 将行和列的积相加
+	                  mov  ax, 0
+	                  mov  es, ax
+	                  mov  di, 200H
 
-	         mov  al, cl          	; 把颜色值给予 al
-	         mov  di, 0
+	                  mov  cx, OFFSET divide_error_end - OFFSET divide_error
 
-	display: 
-	         mov  cx, 0
-	         mov  cl, ds:[si]
-	         jcxz done
+	                  cld                                                   	;DF 重置为零 让di和si可以同时增加1
+	                  rep  movsb                                            	;重复移动
 
-	         mov  es:[bx+di], cl
-	         mov  es:[bx+di+1], al
-	         inc  si
-	         add  di,2
-	         jmp  short display
+	                  ret
 
-	done:    
-	         pop  di
-	         pop  es
-	         pop  cx
-	         pop  bx
-	         pop  ax
-	         ret                  	; pop IP
+
+	divide_error:     
+	                  jmp  short s
+	                  db   'divide error!',0
+
+	s:                mov  ax, 0
+	                  mov  ds,ax
+	                  mov  si,202H
+
+	                  mov  ax, 0B800H
+	                  mov  es, ax
+	                  mov  di, 160*15 + 40*2
+
+	                  mov  cx,0
+
+	show_word:        mov  cl, ds:[si]
+	                  jcxz done
+
+	                  mov  es:[di+0], cl
+	                  mov  byte ptr es:[di+1], 01110001B
+	                  add  di, 2
+	                  inc  si
+	                  jmp  show_word
+									 
+
+	done:             
+	                  mov  ax, 4c00H
+	                  int  21H                                              	; pop IP
+
+	divide_error_end: nop
+
+
+
+
 codesg ends
 
 end start
